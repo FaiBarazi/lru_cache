e1183ca01ff82ae4923d20fbeaa4c4c643acb25c
Faisal Barazi <f.barazi@gmail.com>
2021-10-01 17:44:52 -0700

    Add basic simple lru cache implementation.

---
+++ simple_lru.py
@@ -0,0 +1,27 @@
+from collections import OrderedDict
+
+
+class LRUCache:
+    def __init__(self, max_capacity=1):
+        self.cache = OrderedDict()
+        self.capacity = max_capacity
+
+    def get(self, key):
+        value = self.cache.get(key)
+        if not value:
+            return
+        else:
+            self.cache.move_to_end(key)
+        return value
+
+    def put(self, key, value):
+        self.cache[key] = value
+        self.cache.move_to_end(key)
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False)
+
+    def delete(self, key):
+        self.cache.pop(key, None)
+
+    def reset(self):
+        self.cache = OrderedDict()
